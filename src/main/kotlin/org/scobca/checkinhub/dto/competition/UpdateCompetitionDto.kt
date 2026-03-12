678a72e17b8c31ea4b9b629eebf2519a7ec4301f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-03-13 02:08:35 +0300

    refactor(dto): removed unused variables

--- src/main/kotlin/org/scobca/checkinhub/dto/competition/UpdateCompetitionDto.kt
+++ src/main/kotlin/org/scobca/checkinhub/dto/competition/UpdateCompetitionDto.kt
@@ -3,6 +3,5 @@ package org.scobca.checkinhub.dto.competition
 import org.scobca.checkinhub.interfaces.DtoClass
 
 data class UpdateCompetitionDto(
-    val id: Long,
     val name: String?,
 ) : DtoClass
