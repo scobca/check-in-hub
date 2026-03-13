52090ed76780a33676e6d62bbd2a196e25291abd
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-03-13 13:54:46 +0300

    fix(repository): updated Repository generic type

--- src/main/kotlin/org/scobca/checkinhub/repository/RecordRepository.kt
+++ src/main/kotlin/org/scobca/checkinhub/repository/RecordRepository.kt
@@ -1,8 +1,9 @@
 package org.scobca.checkinhub.repository
 
+import org.scobca.checkinhub.entity.Records
 import org.scobca.checkinhub.repository.specs.SpecsRecordRepository
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RecordRepository : ReactiveCrudRepository<Record, Long>, SpecsRecordRepository
+interface RecordRepository : ReactiveCrudRepository<Records, Long>, SpecsRecordRepository
