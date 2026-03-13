a32905bce5e302119d8c1fa3709421b9954e6dc1
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-03-13 14:33:32 +0300

    fix(mapper): updated mapper signature

--- src/main/kotlin/org/scobca/checkinhub/mapper/RecordMapper.kt
+++ src/main/kotlin/org/scobca/checkinhub/mapper/RecordMapper.kt
@@ -2,8 +2,9 @@ package org.scobca.checkinhub.mapper
 
 import org.mapstruct.Mapper
 import org.scobca.checkinhub.dto.record.CreateRecordDto
+import org.scobca.checkinhub.entity.Records
 
 @Mapper(componentModel = "spring")
 fun interface RecordMapper {
-    fun recordFromDto(dto: CreateRecordDto): Record
+    fun recordFromDto(dto: CreateRecordDto): Records
 }
