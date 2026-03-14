3f708c22bc680b2d1850513d62fa2b31243279a0
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-03-14 12:32:26 +0300

    fix(cors): updated CORS building mechanism

--- src/main/kotlin/org/scobca/checkinhub/config/WebSecurityConfig.kt
+++ src/main/kotlin/org/scobca/checkinhub/config/WebSecurityConfig.kt
@@ -1,14 +1,18 @@
 package org.scobca.checkinhub.config
 
+import org.scobca.checkinhub.config.properties.CorsProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
-class WebSecurityConfig {
+class WebSecurityConfig(private val corsProperties: CorsProperties) {
 
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -21,5 +25,29 @@ class WebSecurityConfig {
             .csrf { it.disable() }
             .logout { it.disable() }
             .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsWebFilter {
+        val corsConfig = CorsConfiguration().apply {
+            addAllowedOrigin(corsProperties.frontendLocalHost)
+            addAllowedOrigin(corsProperties.frontendDockerHost)
+            addAllowedOrigin(corsProperties.frontendProductionHost)
+
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf(
+                "Content-Type",
+                "Authorization",
+                "Accept",
+                "Origin",
+                "X-Requested-With",
+            )
+
+            allowCredentials = true
+        }
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return CorsWebFilter(source)
     }
 }
